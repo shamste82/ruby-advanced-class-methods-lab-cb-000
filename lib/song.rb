47cad93180b370f62c7ffaa739a5b54266d36dd7
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-02 14:35:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,11 @@ class Song
   def self.find_by_name(name)
     @@all.detect {|song| song.name == name}
   end
+
+  def find_or_create_by_name(name)
+    if
+      find_by_name(name)
+    else
+      create_by_name(name)
+    end
 end
