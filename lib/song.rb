3b5042dfbbdb33994a42737610af99a3ccfa33d4
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-02 14:48:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,4 +47,29 @@ class Song
   def self.alphabetical
     @@all.sort_by! { |x| x.name.downcase }
   end
+
+  def new_from_filename(name)
+    rows = name.split("-")
+    puts rows
+  end
 end
+
+#   csv_data = "Elon Musk, 45, Tesla
+# Mark Zuckerberg, 32, Facebook
+# Martha Stewart, 74, MSL
+# "
+#
+# rows = csv_data.split("\n")
+# people = rows.collect do |row|
+#  data = row.split(", ")
+#  name = data[0]
+#  age = data[1]
+#  company = data[2]
+#  person = Person.new
+#  person.name = name
+#  person.age = age
+#  person.company = company
+#  person
+# end
+#
+# end
