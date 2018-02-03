cf7bf11af4b56ae9c1f6bffd198d8d07ca5a3cb9
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-03 08:12:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
     artist = rows[0]
     song_name = rows[1].split(".")
     song_name = song[0]
-    song.name = song
+    song.name = song_name
     song.artist_name = artist
   end
 end
